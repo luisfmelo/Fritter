98ddc9fc2dbd2c4f0dc44769af9a50233d99256f
luisfmelo <luismelo7@gmail.com>
2016-10-21 23:49:29 +0100

    everything is okay to final step

--- public/config/dbconfig.php
+++ public/config/dbconfig.php
@@ -1,8 +1,8 @@
 <?php
-  $host = '';
-  $db = '';
-  $username = '';
-  $password = '';
+  $host = 'db.fe.up.pt';
+  $db = 'siem1629';
+  $username = 'siem1629';
+  $password = 'QmYnvsfl';
 
   error_reporting(0); // Desligar Erros do PHP
 
